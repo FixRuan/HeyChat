ae2b9910da8b55332de1632393ccfbb6c55d601d
fixruan <ruangoio01@gmail.com>
2023-06-16 18:55:18 -0300

    feat: SignIn header

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,10 +1,30 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StatusBar, StyleSheet, Text, View } from "react-native";
+import { CaretLeft } from "phosphor-react-native";
 
 export function SignIn() {
   return (
     <View>
-      <Text>SignIn</Text>
+      <StatusBar barStyle={"dark-content"} backgroundColor={"#fff"} />
+
+      <View style={styles.header}>
+        <CaretLeft size={32} color="#000" />
+        <Text style={styles.headerTitle}>Faça Login</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 16
+  },
+  headerTitle: {
+    fontSize: 24,
+    fontWeight: "500",
+    color: "#000",
+    marginLeft: 12
+  }
+});
