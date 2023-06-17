a0286167c477444b94f7c753b53c4302c91ad6c1
fixruan <ruangoio01@gmail.com>
2023-06-16 23:02:08 -0300

    feat: SignIn screen styles

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StatusBar, StyleSheet, Text, View, TextInput } from "react-native";
+import { StatusBar, StyleSheet, Text, View, TextInput, TouchableOpacity } from "react-native";
 import { CaretLeft } from "phosphor-react-native";
 
 export function SignIn() {
@@ -13,16 +13,38 @@ export function SignIn() {
       </View>
 
       <View style={styles.container}>
-        <Text style={{ fontSize: 32, fontWeight: "bold", color: "#000" }}>HeyChat</Text>
-        <Text style={{ fontSize: 18, color: "#000", marginBottom: 24 }}>Ajude, colabore, faça networking</Text>
+        <Text style={{ fontSize: 32, fontWeight: "bold", color: "#000" }}>
+          HeyChat
+        </Text>
+
+        <Text style={{ fontSize: 18, color: "#000", marginBottom: 24 }}>
+          Ajude, colabore, faça networking
+        </Text>
 
         <View style={styles.form}>
           <TextInput
             style={styles.inputs}
             placeholder="Email"
-            placeholderTextColor={"red"}
+            placeholderTextColor={"#000"}
           />
-          <TextInput style={styles.inputs} placeholder="Senha" />
+
+          <TextInput
+            style={styles.inputs}
+            placeholder="Senha"
+            placeholderTextColor={"#000"}
+          />
+
+          <TouchableOpacity activeOpacity={0.6} style={styles.formButton}>
+            <Text style={{ color: "#fff", fontWeight: "500", fontSize: 20, letterSpacing: 1.4 }}>
+              Login
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity activeOpacity={0.6}>
+            <Text style={{ textAlign: "center", color: "#000", fontSize: 18, fontWeight: "300" }}>
+              Não tem uma conta? crie sua{"\n"} conta agora!
+            </Text>
+          </TouchableOpacity>
         </View>
       </View>
     </View>
@@ -45,7 +67,8 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    marginHorizontal: 24
+    marginHorizontal: 24,
+    marginBottom: 36
   },
   form: {
     width: "100%",
@@ -53,7 +76,20 @@ const styles = StyleSheet.create({
   inputs: {
     width: "100%",
     height: 56,
-    marginBottom: 24,
+    marginBottom: 20,
     color: "#000",
+    borderWidth: 1,
+    borderColor: "#000",
+    padding: 12,
+    borderRadius: 6
+  },
+  formButton: {
+    width: "100%",
+    height: 56,
+    backgroundColor: "#1f6be1",
+    borderRadius: 4,
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 14
   }
 });
