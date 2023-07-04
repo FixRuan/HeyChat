7d3b9fbbebec7ad475a0dba8f85bfa39472ac37e
fixruan <ruangoio01@gmail.com>
2023-07-03 21:42:08 -0300

    feat: add header in chatRoom screen

--- src/screens/ChatRoom/index.tsx
+++ src/screens/ChatRoom/index.tsx
@@ -1,10 +1,33 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { FileSearch } from "phosphor-react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export function ChatRoom() {
   return (
     <View>
-      <Text>ChatRoom</Text>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Grupos</Text>
+        <FileSearch size={32} color="#ffffff" />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    width: "100%",
+    height: 80,
+    backgroundColor: "#1f6be1",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 18,
+    borderBottomLeftRadius: 8,
+    borderBottomRightRadius: 8,
+    marginBottom: 18
+  },
+  headerTitle: {
+    fontSize: 32,
+    color: "#ffffff",
+    fontWeight: "bold"
+  }
+});
