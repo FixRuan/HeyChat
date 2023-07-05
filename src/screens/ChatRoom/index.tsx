c3bb5d60e10bc113a482098393b80277e7889399
fixruan <ruangoio01@gmail.com>
2023-07-04 21:33:03 -0300

    feat: add react-native-responsive-fontsize

--- src/screens/ChatRoom/index.tsx
+++ src/screens/ChatRoom/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FileSearch } from "phosphor-react-native";
 import { StyleSheet, Text, View } from "react-native";
+import { RFValue, RFPercentage } from "react-native-responsive-fontsize";
 
 export function ChatRoom() {
   return (
@@ -16,7 +17,7 @@ export function ChatRoom() {
 const styles = StyleSheet.create({
   header: {
     width: "100%",
-    height: 80,
+    height: RFPercentage(20),
     backgroundColor: "#1f6be1",
     flexDirection: "row",
     justifyContent: "space-between",
@@ -26,7 +27,7 @@ const styles = StyleSheet.create({
     marginBottom: 18
   },
   headerTitle: {
-    fontSize: 32,
+    fontSize: RFValue(32),
     color: "#ffffff",
     fontWeight: "bold"
   }
